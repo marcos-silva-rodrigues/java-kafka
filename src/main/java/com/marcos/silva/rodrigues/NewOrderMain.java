af71b4d33cf89f27cc91477506a492cdb65c0d02
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-04-06 15:26:43 -0300

    feat: partições no kafka para paralelizar processos

--- src/main/java/com/marcos/silva/rodrigues/NewOrderMain.java
+++ src/main/java/com/marcos/silva/rodrigues/NewOrderMain.java
@@ -1,5 +1,6 @@
 package com.marcos.silva.rodrigues;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -8,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
@@ -15,15 +17,27 @@ public class NewOrderMain {
   final Logger logger = LoggerFactory.getLogger(NewOrderMain.class);
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     var producer = new KafkaProducer<String, String>(properties());
-    var value = "123412,1412,2345";
-    var record = new ProducerRecord("ECOMMERCE_NEW_ORDER", value, value);
-    producer.send(record, (data, ex) -> {
-      if(ex != null) {
+
+    var email = "Thanks you for uor order! We are processing your order";
+
+    Callback callback = (data, ex) -> {
+      if (ex != null) {
         ex.printStackTrace();
       } else {
         System.out.println(data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());
       }
-    }).get();
+    };
+
+    for (int i = 0; i < 100; i++) {
+      var key = UUID.randomUUID().toString();
+      var value = key + ",1412,2345";
+      var record = new ProducerRecord("ECOMMERCE_NEW_ORDER", value, value);
+
+      producer.send(record, callback).get();
+      ProducerRecord<String, String> emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+      producer.send(emailRecord,callback).get();
+    }
+
   }
 
   private static Properties properties() {
